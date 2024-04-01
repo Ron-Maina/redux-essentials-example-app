c70bf8768ffd66ba57ed2d17a63733e0a9b35e85
Ron Maina <ronmwangi00@gmail.com>
2024-04-01 15:35:50 +0300

    code cleanup

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -8,20 +8,20 @@ export const SinglePostPage = ({ match }) => {
     state.posts.find(post => post.id === postId)
   )
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
+    if (!post) {
+        return (
+            <section>
+                <h2>Post not found!</h2>
+            </section>
+        )
+    }
 
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <p className="post-content">{post.content}</p>
-      </article>
-    </section>
-  )
+    return (
+        <section>
+            <article className="post">
+                <h2>{post.title}</h2>
+                <p className="post-content">{post.content}</p>
+            </article>
+        </section>
+    )
 }
