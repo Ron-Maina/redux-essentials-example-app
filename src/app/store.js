edf222651b7faa872437cad2125b44dcce46b2c4
Ron Maina <ronmwangi00@gmail.com>
2024-04-01 16:41:26 +0300

    Added usersReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
+import usersreducer from '../features/users/usersSlice'
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer,
+    user: usersreducer
   }
 })
